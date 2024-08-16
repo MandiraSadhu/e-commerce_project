96f17b45d500fc4420301397ea8a3ccfe8ae36cf
Mandira <mandirasadhu@gmail.com>
2024-08-16 18:25:55 +0530

    06:25 : Implemented 'Export products in csv' feature

--- products/views.py
+++ products/views.py
@@ -17,9 +17,12 @@ from django.contrib import messages
 from .utils import AESCipher
 import os
 
-from .tasks import generate_dummy_products
+from .tasks import generate_dummy_products, process_video
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+import csv
+from rest_framework import pagination
 
 class RegisterView(CreateView):
     form_class = UserRegistrationForm
@@ -202,3 +205,47 @@ class GenerateProductsView(generics.GenericAPIView):
                 status=status.HTTP_202_ACCEPTED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+def upload_video(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    if request.method == 'POST':
+        video_file = request.FILES.get('video')
+        if video_file:
+            # Save the uploaded file to the model
+            product.video.save(video_file.name, video_file)
+            
+            # Enqueue the video processing task
+            process_video.delay(product_id=product.id, video_file_path=product.video.path)
+            
+            return Response({"message": "Video upload successful, processing started."}, status=status.HTTP_202_ACCEPTED)
+        return Response({"error": "No video file provided."}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ProductPagination(pagination.PageNumberPagination):
+    page_size = 1000  
+
+class ExportProductsCSV(generics.GenericAPIView):
+    permission_classes = [IsAdmin]
+    pagination_class = ProductPagination
+
+    def get(self, request, *args, **kwargs):
+        paginator = self.pagination_class()
+        products = Product.objects.all()
+        paginated_products = paginator.paginate_queryset(products, request)
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="products.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['ID', 'Category', 'Title', 'Description', 'Price', 'Status', 'Created At', 'Updated At'])
+        for product in paginated_products:
+            writer.writerow([
+                product.id,
+                product.category.name,
+                product.title,
+                product.description,
+                product.price,
+                product.status,
+                product.created_at,
+                product.updated_at
+            ])
+        return response
